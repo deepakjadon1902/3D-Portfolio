a3d96900d86431bfa1ea0ef03d8a0668294e2a21
Deepak Jadon <thakurlakshya0081@gmail.com>
2025-07-05 09:46:08 +0530

    changes in footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -30,7 +30,7 @@ const Footer = () => {
             </div>
           </div>
 
-          {/* Quick Links */}
+          Quick Links
           <div className="space-y-4">
             <h3 className="text-xl font-bold text-white">Quick Links</h3>
             <div className="space-y-2">
