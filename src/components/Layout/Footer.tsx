25996f4a0c15a7dc68c8d3a4eb74615df67f0cea
Deepak Jadon <thakurlakshya0081@gmail.com>
2025-07-05 09:48:06 +0530

    .

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -29,8 +29,6 @@ const Footer = () => {
               </div>
             </div>
           </div>
-
-          Quick Links
           <div className="space-y-4">
             <h3 className="text-xl font-bold text-white">Quick Links</h3>
             <div className="space-y-2">
